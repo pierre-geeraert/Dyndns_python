715a0e6df8848ac9eb2a9f60ea5f5f72d953d516
pierre geeraert <pierre.geeraert@viacesi.fr>
2019-11-03 13:47:48 +0000

    add credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+endpoint = 'your endpoint' #usually ovh-eu
+application_key = 'Your application_key' # find your credentials in https://eu.api.ovh.com/createApp/ and https://api.ovh.com
+application_secret = 'your application_secret'
+consumer_key = 'your consumer_key'
+my_zone = 'domain.com'
+my_subdomain = 'laptop' 
