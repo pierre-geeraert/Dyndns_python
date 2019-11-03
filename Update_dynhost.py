32611177c542360b7501608035f39b202893efc9
pi <pi@pi.fr>
2019-11-03 13:45:15 +0000

    update script

--- Update_dynhost.py
+++ Update_dynhost.py
@@ -19,25 +19,26 @@ client = ovh.Client(
     consumer_key=credential_perso.consumer_key,
 )
 
-
+new_subdomain = credential_perso.my_subdomain
+new_domain = credential_perso.my_zone
 
 def Domain_id():
-    domain_id = client.get('/domain/zone/geeraert.eu/dynHost/record', subDomain='apple')
+    domain_id = client.get('/domain/zone/'+new_domain+'/dynHost/record', subDomain=new_subdomain)
     return domain_id
 
 def update_zone(subdomain,new_ip,domain_id):
-    result = client.put('/domain/zone/geeraert.eu/dynHost/record/'+domain_id,
+    result = client.put('/domain/zone/'+new_domain+'/dynHost/record/'+domain_id,
         ip=new_ip,
         subDomain=subdomain)
     refresh_dns()
     return result
 
 def refresh_dns():
-    result_refresh = client.post('/domain/zone/geeraert.eu/refresh')
+    result_refresh = client.post('/domain/zone/'+new_domain+'/refresh')
     return result_refresh
 
 def MyIP():
     response = requests.get('https://ifconfig.me/ip')
     return response.text
 
-result_update = update_zone('apple',MyIP(),str(Domain_id()[0]))
+result_update = update_zone(new_subdomain,MyIP(),str(Domain_id()[0]))
